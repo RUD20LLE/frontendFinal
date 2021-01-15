6b49e485da147888ca8ab721b512e68a87873d6a
RUD20LLEPS <ruddy20calle@gmail.com>
2021-01-14 22:51:17 -0400

    frontendMQTTfinal

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
 import Inicio from './pages/Inicio';
-import ControlPanel from './pages/ControlPanel';  // Importar ControlPanel
+import ControlPanel from './pages/ControlPanel'; 
 
 function App() {
   return (
@@ -11,7 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Inicio />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/control-panel" element={<ControlPanel />} />  {/* Nueva ruta para ControlPanel */}
+        <Route path="/control-panel" element={<ControlPanel />} /> 
       </Routes>
     </Router>
   );
